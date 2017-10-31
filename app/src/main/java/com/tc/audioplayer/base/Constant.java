fdced8a819d5986a1d42939c195fe00a0392f512
tianxuefeng <tianxuefeng2010@gmail.com>
2017-10-31 16:51:52 +0800

    Constant.java Native ID Interstitial ID Banner ID

--- app/src/main/java/com/tc/audioplayer/base/Constant.java
+++ app/src/main/java/com/tc/audioplayer/base/Constant.java
@@ -8,6 +8,7 @@ public class Constant {
     //Admob App ID: ca-app-pub-9560168446225746~1993710040
     public static final String AdmobNativeID = "ca-app-pub-9560168446225746/8585725473";
     public static final String AdmobInterID = "ca-app-pub-9560168446225746/7376940809";
+    public static final String AdmobBannerID = "ca-app-pub-9560168446225746/1474457993";
 
     private Constant() {
     } // Prevents instantiation
