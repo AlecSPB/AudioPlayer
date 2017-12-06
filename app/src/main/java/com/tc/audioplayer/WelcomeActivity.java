6fd5f0ebba9e8734eff8b7bf79079fb7a0599767
tianxuefeng <tianxuefeng2007@hotmail.com>
2017-12-06 23:36:28 +0800

    Firebase 统计/crash

--- app/src/main/java/com/tc/audioplayer/WelcomeActivity.java
+++ app/src/main/java/com/tc/audioplayer/WelcomeActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.tc.audioplayer.base.ToolbarActivity;
@@ -39,6 +40,8 @@ public class WelcomeActivity extends ToolbarActivity {
         setBgImageFitScreen();
         toolbar.setVisibility(View.GONE);
 
+
+        FirebaseApp.initializeApp(this);
 
         mReadExternalStoragePermission = new ExternalStoragePermission(this);
         mPhoneStatePermission = new PhoneStatePermission(this);
